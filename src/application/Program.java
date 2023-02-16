1efb3ce01343385d0c3dcf833ebf4553aa4fcb0d
Gabriel Kruger <krugergabriel27@outlook.com>
2023-02-16 12:20:49 -0300

    Parking service - With using interface

--- src/application/Program.java
+++ src/application/Program.java
@@ -27,11 +27,12 @@ public class Program {
 		
 		CarRental cr = new CarRental(start, finish, new Vehicle(carModel));
 		
-		System.out.println("Enter price per hour:");
+		System.out.print("Enter price per hour:");
 		double pricePerHour = sc.nextDouble();
-		System.out.println("Enter price per day:");
+		System.out.print("Enter price per day:");
 		double pricePerDay = sc.nextDouble();
 		
+		// Up casting BrTaxService to TaxService
 		RentalService rentalService = new RentalService(pricePerHour, pricePerDay, new BrazilTaxService());
 		
 		rentalService.processInvoice(cr);
